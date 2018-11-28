ccb2b865b8de13653f06f22f1b9caba7e1711e38
Varun Priolkar <me@varunpriolkar.com>
2018-11-28 19:50:37 +0530

    Revert "This PR will fix Issue: 49135 `elasticache_subnet_group`"
    This reverts commit 8fce0903181e21c005e62a3c519486d940ae8a86.

--- lib/ansible/modules/cloud/amazon/elasticache_subnet_group.py
+++ lib/ansible/modules/cloud/amazon/elasticache_subnet_group.py
@@ -58,14 +58,15 @@ EXAMPLES = '''
 '''
 
 try:
-    import boto3
-    import botocore
-    HAS_BOTO3 = True
+    import boto
+    from boto.elasticache import connect_to_region
+    from boto.exception import BotoServerError
+    HAS_BOTO = True
 except ImportError:
-    HAS_BOTO3 = False
+    HAS_BOTO = False
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import HAS_BOTO3, boto3_conn, ec2_argument_spec, get_aws_connection_info
+from ansible.module_utils.ec2 import HAS_BOTO, ec2_argument_spec, get_aws_connection_info
 
 
 def main():
@@ -79,7 +80,7 @@ def main():
     )
     module = AnsibleModule(argument_spec=argument_spec)
 
-    if not HAS_BOTO3:
+    if not HAS_BOTO:
         module.fail_json(msg='boto required for this module')
 
     state = module.params.get('state')
@@ -97,50 +98,49 @@ def main():
                 module.fail_json(msg=str("Parameter %s not allowed for state='absent'" % not_allowed))
 
     # Retrieve any AWS settings from the environment.
+<<<<<<< HEAD
     region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
     import q
     q(aws_connect_kwargs)
+=======
+    region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module)
+>>>>>>> parent of 8fce090318... This PR will fix Issue: 49135 `elasticache_subnet_group`
 
     if not region:
         module.fail_json(msg=str("Either region or AWS_REGION or EC2_REGION environment variable or boto config aws_region or ec2_region must be set."))
 
     """Get an elasticache connection"""
-
-    connection = boto3_conn(module, conn_type='client',
-                            resource='elasticache', region=region,
-                            endpoint=ec2_url, **aws_connect_kwargs)
+    try:
+        conn = connect_to_region(region_name=region, **aws_connect_kwargs)
+    except boto.exception.NoAuthHandlerFound as e:
+        module.fail_json(msg=e.message)
 
     try:
         changed = False
         exists = False
 
         try:
-            matching_groups = connection.describe_cache_subnet_groups(
-                CacheSubnetGroupName=group_name, MaxRecords=100)
+            matching_groups = conn.describe_cache_subnet_groups(group_name, max_records=100)
             exists = len(matching_groups) > 0
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] != 'CacheSubnetGroupNotFoundFault':
-                module.fail_json(msg=e.response['Error']['Message'])
+        except BotoServerError as e:
+            if e.error_code != 'CacheSubnetGroupNotFoundFault':
+                module.fail_json(msg=e.error_message)
 
         if state == 'absent':
             if exists:
-                connection.delete_cache_subnet_group(group_name)
+                conn.delete_cache_subnet_group(group_name)
                 changed = True
         else:
             if not exists:
-                new_group = connection.create_cache_subnet_group(CacheSubnetGroupName=group_name,
-                                                                 CacheSubnetGroupDescription=group_description,
-                                                                 SubnetIds=group_subnets)
+                new_group = conn.create_cache_subnet_group(group_name, cache_subnet_group_description=group_description, subnet_ids=group_subnets)
                 changed = True
             else:
-                changed_group = connection.modify_cache_subnet_group(CacheSubnetGroupName=group_name,
-                                                                     CacheSubnetGroupDescription=group_description,
-                                                                     SubnetIds=group_subnets)
+                changed_group = conn.modify_cache_subnet_group(group_name, cache_subnet_group_description=group_description, subnet_ids=group_subnets)
                 changed = True
 
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Message'] != 'No modifications were requested.':
-            module.fail_json(msg=e.response['Error']['Message'])
+    except BotoServerError as e:
+        if e.error_message != 'No modifications were requested.':
+            module.fail_json(msg=e.error_message)
         else:
             changed = False
 
